3392346fed6653cc6bbc479c59d52e53addc92d8
arnaud35300 <arnaudguillardcontactpro@gmail.com>
2021-05-01 00:02:32 +0200

    1.0.0 done

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #!bin/sh
 
-path=""
+path=$(pwd)
+sed -i 's|path=".*"|path="'"${path}"'"|g' wallpaper.sh
+sed -i 's|path=".*"|path="'"${path}"'"|g' reboot.sh
+crontab -l | grep -v 'wallpaper.sh\|reboot.sh\|DISPLAY=:0' | crontab -
+crontab -l > mycron
+grep -v '^#' config.txt | awk -v path=$path -F ' ' '{print $2,$1,"* * * bash",path"/wallpaper.sh",$3}' >> mycron
+echo -e "DISPLAY=:0\n@reboot dbus-launch bash ${path}/reboot.sh" >> mycron
+crontab mycron
+rm mycron
+
 date_list=($(grep -v '^#' $path/config.txt | awk -F ' ' '{print $1*3600+$2*60}' | sort -n))
 current_date=$(date +%H:%M | awk -F ':' '{print $1*3600+$2*60}')
 last_date=-1
@@ -15,5 +24,5 @@ if [ "$last_date" -eq "-1" ]
 then
 	last_date=date_list[-1]
 fi
-export $(cat /proc/$(pgrep -u `whoami` ^gnome-shell$)/environ | grep -z DBUS_SESSION_BUS_ADDRESS)
 gsettings set org.gnome.desktop.background picture-uri "file://${path}/wallpapers/$(grep "^$(date -d@$last_date -u +%H:%M | tr ':' ' ')" $path/config.txt | cut -d' ' -f3)"
+
--- wallpaper.sh
+++ wallpaper.sh
@@ -1,7 +1,5 @@
 #!bin/sh
 
-# You can specify your own directory.
-path="/home/arnaud/Documents/shell_script/dynamic_wallpapers"
-target_dir="file://${path}/wallpapers/"
+path=""
 export $(cat /proc/$(pgrep -u `whoami` ^gnome-shell$)/environ | grep -z DBUS_SESSION_BUS_ADDRESS)
-gsettings set org.gnome.desktop.background picture-uri "${target_dir}${1}"
+gsettings set org.gnome.desktop.background picture-uri "file://${path}/wallpapers/${1}"
